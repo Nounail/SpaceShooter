c4a8d24b00026154aa24391691d9432dab29df79
Nounail <122325232+Nounail@users.noreply.github.com>
2023-09-24 00:12:13 -0500

    2.4

---
+++ Player/Player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+var speed = 5
+var max_speed = 1000	 
+var rotate_speed = 0.08
+var nose = Vector2(0, -60)
+var health = 10
+var Bullet = load("res://Player/bullet.tscn")
+var Effects = null
+var Explosion = load('res://Effects/explosion.tscn')
+
+func get_input():
+	var to_return = Vector2.ZERO
+	if Input.is_action_pressed("Forward"):
+		to_return += Vector2(0, -1)
+		$Exhaust.show()
+	else:
+		$Exhaust.hide()
+
+	if Input.is_action_pressed("Left"):
+		rotation -= rotate_speed
+	if Input.is_action_pressed("Right"):
+		rotation += rotate_speed
+
+	return to_return.rotated(rotation) 
+
+func _physics_process(_delta):
+	velocity += get_input() * speed
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	
+	position.x = wrapf(position.x, 0, 1152)
+	position.y = wrapf(position.y, 0, 648)
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	
+	print(velocity.length())
+	
+	move_and_slide()
+	
+	if Input.is_action_pressed("Shoot"): 
+		var bullet = Bullet.instantiate()
+		bullet.position = position + nose.rotated(rotation)
+		bullet.rotation = rotation
+		var Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null: 
+			Effects.add_child(bullet)
+
+func damage(d):
+	health = health - d
+	if health <= 0:
+		Effects = get_node_or_null("/root/Game/Effects")
+		if Effects != null:
+			var explosion = Explosion.instantiate()
+			Effects.add_child(explosion)
+			explosion.global_position = global_position
+			hide()
+			await explosion.animation_finished
+		Global.update_lives(-1)
+		queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	if body.name != "Player":
+		damage(100) 
